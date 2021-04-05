bbd956a01684f92c3b435beff74a442deeb71e8d
196cj6y7 <43287205+196cj6y7@users.noreply.github.com>
2021-04-05 11:04:37 +0800

    Add files via upload

--- rootloci.py
+++ rootloci.py
@@ -7,12 +7,17 @@ Created on Sat Apr  3 17:23:56 2021
 
 from control import *
 from matplotlib import pyplot as plt
-
+def plotRootloci(transferFunction):      
+    rlocus(transferFunction)
+    plt.title('rootloci')
+    plt.xlabel('Re(s)')
+    plt.ylabel('Im(s)')
+    plt.show()
+    
 '''
     G(s)H(s) = K*1/(4s^3 + 4s^2 + s)
 '''
 
-#sy1 = tf([1], [4,4,1,0])
-sy1 = tf([1,4],[1,1,-2])
-rlocus(sy1)
-plt.show()
+G = tf([1,4],[1,1,-2])
+plotRootloci(G)
+
